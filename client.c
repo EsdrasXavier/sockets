b3c2f7e5be98856e1d4780b7c0edbd15b85bd1ea
EsdrasXavier <esdrasxa@gmail.com>
2020-05-29 00:07:13 +0000

    Conexão ao sistema.

--- client.c
+++ client.c
@@ -1,16 +1,28 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <sys/socket.h>
 
-
 int main(int argc, char *argv[]) {
+  int socketDesc;
+  struct sockaddr_in server;
 
-    // AF_INET - IPv4, SOCK_STREAM - tcp, 0 - IP
-    int socketDesc = socket(AF_INET, SOCK_STREAM, 0);
+  // AF_INET - IPv4, SOCK_STREAM - tcp, 0 - IP
+  socketDesc = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (socketDesc == -1) {
-        printf("Não foi possivel criar o socket.");
-        return 1;
-    }
+  if (socketDesc == -1) {
+    printf("Não foi possivel criar o socket.\n");
+    return 1;
+  }
 
-    return 0;
+  server.sin_addr.s_addr = inet_addr("172.217.28.132");
+  server.sin_family = AF_INET;
+  server.sin_port = htons(80);
+
+  if (connect(socketDesc, (struct sockaddr *)&server, sizeof(server)) == -1) {
+    printf("Não foi possivel conectar-se.\n");
+    return 1;
+  }
+
+  return 0;
 }
